bf290fad4fc00a2bfc8e65e75347795948239e86
AdrianTofan <adrianvasile.tofan@gmail.com>
2020-03-02 02:16:02 +0000

    +Animations

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,19 +5,24 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private float flaps = 0;
-    float speed = 15f;
-    float glideSpeed = 20f;
+    float speed = 20f;
+    float glideSpeed = 25f;
     float flapHeight = 2f;
     Vector3 flapVector;
     Vector3 glideVector;
     int pressed = 0;
     float xRot = 0.2f;
+    float rotationSpeed = 70f;
     bool lost = false;
     public CharacterController controller;
-    
+    public Animator anim;
+    Quaternion originalRotation;
+
     // Start is called before the first frame update
     void Start()
     {
+        anim = GetComponent<Animator>();
+        originalRotation = transform.rotation;
         /*
         v =  (Vector3.up + Vector3.forward).normalized * speed;
         GetComponent<Rigidbody>().velocity = v;
@@ -46,8 +51,15 @@ public class PlayerMovement : MonoBehaviour
             */
             flapVector = (Vector3.up + Vector3.forward).normalized * speed;
             GetComponent<Rigidbody>().velocity = flapVector;
+            anim.Play("FlapForward");
+            anim.SetInteger("condition", 1);
 
 
+        }
+
+        if(Input.GetKeyUp(KeyCode.Space))
+        {
+            anim.SetInteger("condition", 0);
         }
     }
 
@@ -59,8 +71,8 @@ public class PlayerMovement : MonoBehaviour
 
 
 
+            anim.SetInteger("condition", 2);
 
-            
             glideVector = (Vector3.forward  - Vector3.up).normalized * glideSpeed;
             GetComponent<Rigidbody>().velocity = glideVector;
             
@@ -69,13 +81,16 @@ public class PlayerMovement : MonoBehaviour
             transform.Rotate(xRot, 0f, 0f);
             Debug.Log("Gliding.");
             glideSpeed += 0.2f;
-            
+            anim.Play("GlideForward");
+
         }
         if(Input.GetKeyUp(KeyCode.E))
         {
-           // glideSpeed = 3f;
-           
-            transform.Rotate(0f, 0f, 0f);
+            // glideSpeed = 3f;
+
+            transform.rotation = Quaternion.Slerp(transform.rotation, originalRotation, Time.deltaTime * rotationSpeed);
+           // anim.Play("HoverUp");
+            anim.SetInteger("condition", 3);
         }
 
 
